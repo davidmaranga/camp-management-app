9671843e6548f0cae85cb10598b89d034acf444a
Sly Kint A. Bacalso <64179667+Son0fAnton@users.noreply.github.com>
2021-10-06 08:42:43 +0800

    chunk components

---
+++ src/utils/sprite.js
@@ -0,0 +1,21 @@
+async function dataLocation(map, lat, long) {
+  //   map.current.flyTo({
+  //     center: [long, lat],
+  //     speed: 0.5,
+  //   });
+
+  return {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [long, lat],
+        },
+      },
+    ],
+  };
+}
+
+export { dataLocation };
